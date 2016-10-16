95d756ad9c261d3f77b77f989e293386a6297433
Benoit Jacob <benoitjacob@google.com>
2016-10-16 17:50:44 -0400

    Comment fixedpoint.h.

--- internal/fixedpoint.h
+++ internal/fixedpoint.h
@@ -15,17 +15,6 @@
 // fixedpoint.h: fixed-point arithmetic, with basic operations and
 // a few math functions such as tanh.
 
-// This is only used in output.h
-// for some specific output pipeline stages (tanh); most of gemmlowp
-// uses only plain integer arithmetic, not fixed-point arithmetic.
-// At the most basic level, we distinguish between plain integer
-// arithmetic and fixed-point arithmetic by the type of multiplication
-// that is used: plain integer arithmetic uses plain (overflowing)
-// integer multiplication, whereas fixed-point arithmetic uses
-// "multiply-high" instructions, which means using only the most
-// significant bits of the product, or equivalently, multiplying
-// fixed-point numbers in the [-1 .. +1] interval.
-
 #ifndef GEMMLOWP_INTERNAL_FIXEDPOINT_H_
 #define GEMMLOWP_INTERNAL_FIXEDPOINT_H_
 
@@ -36,108 +25,160 @@
 
 namespace gemmlowp {
 
+// Part 1: Low-level integer-arithmetic primitives.
+// The implementations here are generic implementations valid for
+// scalar types (e.g. int32_t). Architecture-specific SIMD types
+// (e.g. NEON int32x4_t) may be supported by providing
+// specializations for them in separate files.
+//
+// The purpose of these primitives is two-fold:
+//  - They will be used to implement higher-level fixed-point
+//    abstractions, namely the FixedPoint class and its arithmetic
+//    operators.
+//  - They will be directly used to implement some more involved
+//    fixed-point computations, e.g. the fixed-point implementation
+//    of math functions such as tanh.
+
+// Plain bit-wise AND
 template <typename tIntegerType>
 tIntegerType BitAnd(tIntegerType a, tIntegerType b) {
   return a & b;
 }
 
+// Plain bit-wise OR
 template <typename tIntegerType>
 tIntegerType BitOr(tIntegerType a, tIntegerType b) {
   return a | b;
 }
 
+// Plain bit-wise XOR
 template <typename tIntegerType>
 tIntegerType BitXor(tIntegerType a, tIntegerType b) {
   return a ^ b;
 }
 
+// Plain bit-wise NOT
 template <typename tIntegerType>
 tIntegerType BitNot(tIntegerType a) {
   return ~a;
 }
 
+// Integer addition. Not saturating. Overflow is undefined behavior.
 template <typename tIntegerType>
 tIntegerType Add(tIntegerType a, tIntegerType b) {
   return a + b;
 }
 
+// Integer subtraction. Not saturating. Overflow is undefined behavior.
 template <typename tIntegerType>
 tIntegerType Sub(tIntegerType a, tIntegerType b) {
   return a - b;
 }
 
+// Integer unary negative. Not saturating. Overflow is undefined behavior.
 template <typename tIntegerType>
 tIntegerType Neg(tIntegerType a) {
   return -a;
 }
 
+// Integer arithmetic left-shift, equivalent to multiplying with a
+// power of two. Not saturating. Overflow is undefined behavior.
 template <typename tIntegerType>
 tIntegerType ShiftLeft(tIntegerType a, int offset) {
   return a * (1 << offset);
 }
 
+// Integer arithmetic right-shift, equivalent to dividing by a
+// power of two. Rounds towards zero, like an ordinary integer division.
 template <typename tIntegerType>
 tIntegerType ShiftRight(tIntegerType a, int offset) {
   return a / (1 << offset);
 }
 
+// Each bit of the result is set to the corresponding bit of either then_val or
+// else_val depending on whether the corresponding bit of if_mask is set.
+// Equivalent to the VBSL instruction in ARM NEON.
 template <typename tIntegerType>
 tIntegerType SelectUsingMask(tIntegerType if_mask, tIntegerType then_val,
                              tIntegerType else_val) {
   return BitXor(BitAnd(if_mask, then_val), BitAnd(BitNot(if_mask), else_val));
 }
 
+// For each input scalar, the corresponding bits of the result are set if the
+// input scalar is non-zero.
 template <typename tIntegerType>
 tIntegerType MaskIfNonZero(tIntegerType a) {
   static const tIntegerType zero = 0;
   return a ? BitNot(zero) : zero;
 }
 
+// For each input scalar, the corresponding bits of the result are set if the
+// input scalar is zero.
 template <typename tIntegerType>
 tIntegerType MaskIfZero(tIntegerType a) {
   return MaskIfNonZero<tIntegerType>(!a);
 }
 
+// For each pair of input scalars, the corresponding bits of the result are
+// set if the input scalars are equal.
 template <typename tIntegerType>
 tIntegerType MaskIfEqual(tIntegerType a, tIntegerType b) {
   return MaskIfNonZero<tIntegerType>(a == b);
 }
 
+// For each pair of input scalars, the corresponding bits of the result are
+// set if the input scalars are not equal.
 template <typename tIntegerType>
 tIntegerType MaskIfNotEqual(tIntegerType a, tIntegerType b) {
   return MaskIfNonZero<tIntegerType>(a != b);
 }
 
+// For each pair of input scalars, the corresponding bits of the result are
+// set if the input scalars a, b satisfy a > b.
 template <typename tIntegerType>
 tIntegerType MaskIfGreaterThan(tIntegerType a, tIntegerType b) {
   return MaskIfNonZero<tIntegerType>(a > b);
 }
 
+// For each pair of input scalars, the corresponding bits of the result are
+// set if the input scalars a, b satisfy a >= b.
 template <typename tIntegerType>
 tIntegerType MaskIfGreaterThanOrEqual(tIntegerType a, tIntegerType b) {
   return MaskIfNonZero<tIntegerType>(a >= b);
 }
 
+// For each pair of input scalars, the corresponding bits of the result are
+// set if the input scalars a, b satisfy a < b.
 template <typename tIntegerType>
 tIntegerType MaskIfLessThan(tIntegerType a, tIntegerType b) {
   return MaskIfNonZero<tIntegerType>(a < b);
 }
 
+// For each pair of input scalars, the corresponding bits of the result are
+// set if the input scalars a, b satisfy a <= b.
 template <typename tIntegerType>
 tIntegerType MaskIfLessThanOrEqual(tIntegerType a, tIntegerType b) {
   return MaskIfNonZero<tIntegerType>(a <= b);
 }
 
+// Returns true if all of the input scalars are nonzero.
+// This function may currently assume that each of the input scalars has either
+// all or none of its bits set. Otherwise, its behavior is currently undefined.
 template <typename tIntegerType>
 bool All(tIntegerType a) {
   return a;
 }
 
+// Returns true if any of the input scalars are nonzero.
+// This function may currently assume that each of the input scalars has either
+// all or none of its bits set. Otherwise, its behavior is currently undefined.
 template <typename tIntegerType>
 bool Any(tIntegerType a) {
   return a;
 }
 
+// Returns (a+b)/2, rounded to the nearest integer.
+// Equivalent to VRHADD in the ARM NEON instruction set.
 template <typename IntegerType>
 IntegerType RoundingHalfSum(IntegerType a, IntegerType b) {
   static_assert(std::is_same<IntegerType, void>::value, "unimplemented");
@@ -153,6 +194,34 @@ inline int32_t RoundingHalfSum(int32_t a, int32_t b) {
   return static_cast<int32_t>((sum + sign) / 2);
 }
 
+// Returns the integer that represents the product of two fixed-point
+// numbers, interpreting all integers as fixed-point values in the
+// interval [-1, 1), rounding to the nearest value, and saturating
+// -1 * -1 to the maximum value (since 1 is not in the half-open
+// interval [-1, 1)).
+//
+// [The explanation below specializes to int32_t for example purpose.]
+//
+// The mapping between IntegerType and the interval [-1, 1) is unique and
+// implied by IntegerType, which is assumed to be signed. For example,
+// for IntergerType==int32_t, the mapping is
+//   real_value = integer_value / 2^31.
+// So in this case, and leaving aside rounding and saturating, this
+// function computes ((a / 2^31) * (b / 2^31)) * 2^31, which simplifies to
+//   (a * b) / 2^31.
+//
+// The 'doubling' part in the name of this function comes from the fact that
+// this operation is very close to a "multiply-high" operation, keeping only
+// the top half bits, except that that would be effectively computing
+//   (a * b) / 2^32,
+// so here we are computing 2x that, since
+//   1/2^31 = 2 * 1/2^32.
+// The idea is to use all of the available 32 bits in the destination int32
+// value.
+//
+// [End of the explanation specializing to int32.]
+//
+// This is equivalent to the VQRDMULH instruction in ARM NEON.
 template <typename IntegerType>
 IntegerType SaturatingRoundingDoublingHighMul(IntegerType a, IntegerType b) {
   static_assert(std::is_same<IntegerType, void>::value, "unimplemented");
@@ -172,6 +241,10 @@ inline int32_t SaturatingRoundingDoublingHighMul(int32_t a, int32_t b) {
   return overflow ? std::numeric_limits<int32_t>::max() : ab_x2_high32;
 }
 
+// Returns the product of a run-time integer value by a compile-time power
+// of two, with either a positive exponent (equivalent to an arithmetic
+// left shift, saturating) or a negative exponent (equivalent to an arithmetic
+// right shift, rounding to nearest).
 template <int Exponent, typename IntegerType,
           int ExponentSign = (Exponent > 0 ? 1 : Exponent < 0 ? -1 : 0)>
 struct ImplSaturatingRoundingMultiplyByPOT {};
@@ -206,6 +279,8 @@ IntegerType SaturatingRoundingMultiplyByPOT(IntegerType x) {
   return ImplSaturatingRoundingMultiplyByPOT<Exponent, IntegerType>::eval(x);
 }
 
+// Some compile-time traits around raw types to handle SIMD aspects:
+// number of lanes, underlying scalar type.
 template <typename tIntegerType>
 struct FixedPointRawTypeTraits {};
 
@@ -215,11 +290,41 @@ struct FixedPointRawTypeTraits<int32_t> {
   static const int kLanes = 1;
 };
 
+// Returns a SIMD value duplicating a scalar value across all lanes.
 template <typename tRawType>
 tRawType Dup(typename FixedPointRawTypeTraits<tRawType>::ScalarRawType x) {
   return x;
 }
 
+// Part 2: the FixedPoint class.
+
+// A FixedPoint object represents a fixed-point value stored in the underlying
+// integer type tRawType, if tRawType is a plain scalar integer type.
+// Alternatively, tRawType may be a SIMD type (e.g. NEON int32x4_t) in which
+// case a FixedPoint object represents a corresponding SIMD vector of fixed
+// point values.
+//
+// tIntegerBits describes the range of the fixed-point format: if
+// tIntegerBits == m then the range of representable values is the half-open
+// interval [-2^m; 2^m) where the open boundary on the right side means that
+// 2^m is not representable (how close the maximum representable value is to
+// it, depends on bit-depth of tRawType).
+//
+// In "Q format notation",
+//   https://en.wikipedia.org/wiki/Q_(number_format)
+// we are describing the format
+//   Qm.n
+// where
+//   m = tIntegerBits
+// and
+//   n = NumberOfBits(tRawType) - (m + 1)
+// Note that the (m + 1) in the above line is because we adopt the convention
+// that we count the integer bits exclusively of the sign bit; so (m + 1) is
+// the total number of integer bits inclusive of the sign bit.
+//
+// Accordingly, the number of integral representable values in our range
+//   [-2^m ; 2^m)
+// is equal to 2^(m+1).
 template <typename tRawType, int tIntegerBits>
 class FixedPoint {
  public:
@@ -292,6 +397,13 @@ class FixedPoint {
   RawType i_;
 };
 
+// Part 3: implementation of arithmetic operators for the
+// FixedPoint class, and a few related functions.
+
+// A FixedPoint multiplication is just a
+// SaturatingRoundingDoublingHighMul operation on the underlying
+// raw integer values. The IntegerBits simply add up, as is obvious
+// from the fact that the range is [-2^IntegerBits, 2^IntegerBits).
 template <typename tRawType, int tIntegerBits_a, int tIntegerBits_b>
 FixedPoint<tRawType, tIntegerBits_a + tIntegerBits_b> operator*(
     FixedPoint<tRawType, tIntegerBits_a> a,
@@ -301,6 +413,7 @@ FixedPoint<tRawType, tIntegerBits_a + tIntegerBits_b> operator*(
   return c;
 }
 
+// Tweaking IntegerBits gives exact multiplication by a power of two.
 template <int tExponent, typename tRawType, int tIntegerBits>
 FixedPoint<tRawType, tExponent + tIntegerBits> ExactMulByPot(
     FixedPoint<tRawType, tIntegerBits> a) {
@@ -309,12 +422,16 @@ FixedPoint<tRawType, tExponent + tIntegerBits> ExactMulByPot(
   return c;
 }
 
+// If we want to leave IntegerBits fixed, then multiplication
+// by a power of two has to be saturating/rounding, not exact anymore.
 template <int tExponent, typename tRawType, int tIntegerBits>
 FixedPoint<tRawType, tIntegerBits> SaturatingRoundingMultiplyByPOT(
     FixedPoint<tRawType, tIntegerBits> a) {
   return FixedPoint<tRawType, tIntegerBits>::FromRaw(
       SaturatingRoundingMultiplyByPOT<tExponent>(a.raw()));
 }
+
+// Generic arithmetic operators.
 
 #define MAKE_FIXEDPOINT_UNARY_FUNC(FuncName, ImplFuncName)                     \
   template <typename tRawType, int tIntegerBits>                               \
@@ -389,6 +506,8 @@ bool operator!=(FixedPoint<tRawType, tIntegerBits> a,
   return !(a == b);
 }
 
+// Conversions.
+
 template <typename tRawType, int tIntegerBits>
 double ToDouble(FixedPoint<tRawType, tIntegerBits> x) {
   static_assert(FixedPointRawTypeTraits<tRawType>::kLanes == 1,
@@ -406,6 +525,8 @@ FixedPoint<tRawType, tIntegerBits> ToFixedPoint(double x) {
                double(F::ScalarRawMax()))));
 }
 
+// Rescale changes the number of IntegerBits and updates the underlying
+// raw integer value accordingly.
 template <int tIntegerBitsDst, typename tRawType, int tIntegerBitsSrc>
 FixedPoint<tRawType, tIntegerBitsDst> Rescale(
     FixedPoint<tRawType, tIntegerBitsSrc> x) {
@@ -415,6 +536,10 @@ FixedPoint<tRawType, tIntegerBitsDst> Rescale(
   return result;
 }
 
+// CheckedFixedPointConstant allows to specify fixed-point constants
+// initialized as real numbers, in a way that does not compile floating-point
+// arithmetic in production code, yet still checks agreement with the
+// floating-point expressions when asserts are enabled.
 #ifdef GEMMLOWP_ENABLE_FIXEDPOINT_CONSTANTS_CHECKS
 template <typename FixedPointType>
 FixedPointType CheckedFixedPointConstant(
@@ -436,6 +561,9 @@ FixedPointType CheckedFixedPointConstant(
   (FixedPointType::FromScalarRaw(ScalarRawValue))
 #endif
 
+// Implementation of exponential function.
+
+// Returns exp(x) for x in [-1/4, 0).
 template <typename tRawType>
 FixedPoint<tRawType, 0> exp_on_interval_between_negative_one_quarter_and_0_excl(
     FixedPoint<tRawType, 0> a) {
@@ -459,6 +587,7 @@ FixedPoint<tRawType, 0> exp_on_interval_between_negative_one_quarter_and_0_excl(
          constant_term * (x + x4_over_24_plus_x3_over_6_plus_x2_over_2);
 }
 
+// Returns exp(x) for x < 0.
 template <typename tRawType, int tIntegerBits>
 FixedPoint<tRawType, 0> exp_on_negative_values(
     FixedPoint<tRawType, tIntegerBits> a) {
@@ -504,6 +633,9 @@ FixedPoint<tRawType, 0> exp_on_negative_values(
   return result;
 }
 
+// Implementation of tanh.
+
+// Returns (1 - x) / (1 + x) for x in [0, 1).
 template <typename tRawType>
 FixedPoint<tRawType, 0> one_minus_x_over_one_plus_x_for_x_in_0_1(
     FixedPoint<tRawType, 0> a) {
@@ -524,6 +656,7 @@ FixedPoint<tRawType, 0> one_minus_x_over_one_plus_x_for_x_in_0_1(
   return Rescale<0>(x - F2::One());
 }
 
+// Returns -tanh(x) for x < 0.
 template <typename tRawType, int tIntegerBits>
 FixedPoint<tRawType, 0> neg_tanh_on_negative_values(
     FixedPoint<tRawType, tIntegerBits> a) {
@@ -531,6 +664,7 @@ FixedPoint<tRawType, 0> neg_tanh_on_negative_values(
       exp_on_negative_values(ExactMulByPot<1>(a)));
 }
 
+// Returns tanh(x) for any x.
 template <typename tRawType, int tIntegerBits>
 FixedPoint<tRawType, 0> tanh(FixedPoint<tRawType, tIntegerBits> a) {
   typedef FixedPoint<tRawType, tIntegerBits> InputF;
