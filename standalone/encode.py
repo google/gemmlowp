18e4cc012a21279b6fe4a24e7988ecdc3445cf5a
Benoit Jacob <jacob.benoit.1@gmail.com>
2018-11-12 22:24:00 -0500

    support encoding sdot as well as udot

--- standalone/encode.py
+++ standalone/encode.py
@@ -12,11 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
-"""
-Encodes ARM asm code for certain instructions into the corresponding machine
-code encoding, as a .word directive in the asm code, preserving the original
-code in a comment.
+"""Encodes ARM asm code for certain instructions into the corresponding machine code encoding, as a .word directive in the asm code, preserving the original code in a comment.
 
 Reads from stdin, writes to stdout.
 
@@ -32,49 +28,53 @@ import sys
 import re
 
 
-def encode_udot_vector(line):
+def encode_udot_sdot_vector(line):
   m = re.search(
-      r'\budot[ ]+v([0-9]+)[ ]*.[ ]*4s[ ]*,[ ]*v([0-9]+)[ ]*.[ ]*16b[ ]*,[ ]*v([0-9]+)[ ]*.[ ]*16b',
+      r'\b([us])dot[ ]+v([0-9]+)[ ]*\.[ ]*4s[ ]*\,[ ]*v([0-9]+)[ ]*\.[ ]*16b[ ]*\,[ ]*v([0-9]+)[ ]*\.[ ]*16b',
       line)
   if not m:
     return 0, line
 
   match = m.group(0)
-  accum = int(m.group(1))
-  lhs = int(m.group(2))
-  rhs = int(m.group(3))
+  unsigned = 1 if m.group(1) == 'u' else 0
+  accum = int(m.group(2))
+  lhs = int(m.group(3))
+  rhs = int(m.group(4))
   assert accum >= 0 and accum <= 31
   assert lhs >= 0 and lhs <= 31
   assert rhs >= 0 and rhs <= 31
-  mcode = 0x6e809400 | (accum << 0) | (lhs << 5) | (rhs << 16)
+  mcode = 0x4e809400 | (accum << 0) | (lhs << 5) | (rhs << 16) | (
+      unsigned << 29)
   return mcode, match
 
 
-def encode_udot_element(line):
+def encode_udot_sdot_element(line):
   m = re.search(
-      r'\budot[ ]+v([0-9]+)[ ]*.[ ]*4s[ ]*,[ ]*v([0-9]+)[ ]*.[ ]*16b[ ]*,[ ]*v([0-9]+)[ ]*.[ ]*4b[ ]*\[([0-9])\]',
+      r'\b([us])dot[ ]+v([0-9]+)[ ]*\.[ ]*4s[ ]*\,[ ]*v([0-9]+)[ ]*\.[ ]*16b[ ]*\,[ ]*v([0-9]+)[ ]*\.[ ]*4b[ ]*\[([0-9])\]',
       line)
   if not m:
     return 0, line
 
   match = m.group(0)
-  accum = int(m.group(1))
-  lhs = int(m.group(2))
-  rhs = int(m.group(3))
-  lanegroup = int(m.group(4))
+  unsigned = 1 if m.group(1) == 'u' else 0
+  accum = int(m.group(2))
+  lhs = int(m.group(3))
+  rhs = int(m.group(4))
+  lanegroup = int(m.group(5))
   assert accum >= 0 and accum <= 31
   assert lhs >= 0 and lhs <= 31
   assert rhs >= 0 and rhs <= 31
   assert lanegroup >= 0 and lanegroup <= 3
   l = 1 if lanegroup & 1 else 0
   h = 1 if lanegroup & 2 else 0
-  mcode = 0x6f80e000 | (accum << 0) | (lhs << 5) | (rhs << 16) | (l << 21) | (
-      h << 11)
+  mcode = 0x4f80e000 | (accum << 0) | (lhs << 5) | (rhs << 16) | (l << 21) | (
+      h << 11) | (
+          unsigned << 29)
   return mcode, match
 
 
 def encode(line):
-  for encode_func in [encode_udot_vector, encode_udot_element]:
+  for encode_func in [encode_udot_sdot_vector, encode_udot_sdot_element]:
     mcode, match = encode_func(line)
     if mcode:
       return mcode, match
